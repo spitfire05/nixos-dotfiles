aa55b55dc49efe02f4412e7aa57643dac7e4da9d
Michal Borejszo <michal.borejszo@pm.me>
2026-08-29 22:53:44 +0200

    remove unsupported x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -64,7 +64,6 @@
       "x86_64-linux"
       "aarch64-linux"
       "aarch64-darwin"
-      "x86_64-darwin"
     ];
     forAllSystems = nixpkgs.lib.genAttrs devSystems;
     pkgsFor = system: nixpkgs.legacyPackages.${system};
