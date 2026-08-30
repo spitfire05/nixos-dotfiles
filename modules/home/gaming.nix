3ce50c93609e8545dc93da901994ba519bd0c18e
Michal Borejszo <michal.borejszo@pm.me>
2026-08-30 21:09:05 +0200

    fix gamerun for moonshine HDR

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -23,8 +23,6 @@
     gamescope
 
     # Custom wrapper to run games with gamemoderun + mangohud + selected Proton envs.
-    # When MOONSHINE_CLIENT_FRAMERATE is present (i.e. the game was launched by
-    # Moonshine for a remote client), we addiotnally wrap the game in gamescope.
     (writeShellScriptBin "gamerun" ''
       if [ $# -eq 0 ]; then
         printf 'Usage: %s command [args...]\n' "''${0##*/}" >&2
@@ -36,19 +34,14 @@
         "PROTON_USE_NTSYNC=1"
         "DXVK_ASYNC=1"
         "PROTON_ENABLE_NVAPI=1"
-        "PROTON_ENABLE_WAYLAND=1"
       )
 
       if [ -z "''${MOONSHINE_CLIENT_FRAMERATE:-}" ]; then
+        ENV+=("PROTON_ENABLE_WAYLAND=1")
         exec gamemoderun mangohud env "''${ENV[@]}" "''$@"
       else
-        exec gamemoderun env "''${ENV[@]}" gamescope -f \
-        -W "''$MOONSHINE_CLIENT_WIDTH" \
-        -H "''$MOONSHINE_CLIENT_HEIGHT" \
-        -w "''$MOONSHINE_CLIENT_WIDTH" \
-        -h "''$MOONSHINE_CLIENT_HEIGHT" \
-        -r "''$MOONSHINE_CLIENT_FRAMERATE" \
-        -- "''$@"
+        ENV+=("PROTON_ENABLE_HDR=1")
+        exec gamemoderun env "''${ENV[@]}" "''$@"
       fi
     '')
   ];
