9fec662ac90498da46d63c4494681078e6183e4c
Michal Borejszo <michal.borejszo@pm.me>
2026-08-31 22:35:37 +0200

    Revert "drop unneeded gamescope"
    This reverts commit 2de77e14890af023ea32afd81cdf07eff0ca0ac2.

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -20,6 +20,8 @@
   };
 
   home.packages = with pkgs; [
+    gamescope
+
     # Custom wrapper to run games with gamemoderun + mangohud + selected Proton envs.
     (writeShellScriptBin "gamerun" ''
       if [ $# -eq 0 ]; then
